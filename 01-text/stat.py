3749f30081e9ad47014414191328eddaafd0b956
Marek Valko <valko.marek@gmail.com>
2018-09-21 10:28:32 +0200

    Cleaned up, added comments

--- 01-text/stat.py
+++ 01-text/stat.py
@@ -8,34 +8,30 @@ composer = Counter()
 
 
 for line in open(sys.argv[1],"r"):
-    # print(line)
-    
     if sys.argv[2] == "composer":
-        # rozdelit na bodkociarkach, zrusit roky
         r = re.compile(r"Composer: (.*)")
         m = r.match(line)
+        # if line doesnt start with Composer, skip
         if m is None:
             continue
-        # print(m.group(1))
         rawcomp = m.group(1)
         comp = rawcomp.split(";")
+        # try every composer after spliting by ";
         for c in comp:
             if not c:
                 continue
             c.strip()
+            # match and delete () with year
             s = re.compile(r"(.*) \(")
             n = s.match(c)
             if n is not None:
+                # if matched, use that version
                 composer[n.group(1).strip()] += 1
             else:
+                #otherwise use version before matching
                 composer[c.strip()] += 1
-        # print(comp)
-        # composer[comp] +=1
-
-# print(composer)
+        
 if sys.argv[2] == "composer":
     for c,n in composer.items():
         print(c + ": "+ str(n))
         
-
-# print("Hello world")
