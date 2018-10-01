70cd041c8ecc36584efd023c06111ea2a08d6d2b
Marek Valko <valko.marek@gmail.com>
2018-10-01 22:10:01 +0200

    finnished format method

--- 02-class/scorelib.py
+++ 02-class/scorelib.py
@@ -4,11 +4,56 @@ class Print:
         self.print_id = print_id
         self.partiture = partiture
     
+    # maybe DONE
     def format(self):
-        pass
+        print("Print Number: "+str(self.print_id))
+        
+        if self.edition.composition.authors:
+            persons = ""
+            for person in self.edition.composition.authors:
+                if person.born is not None:
+                    if person.died is not None:
+                        persons += "{} ({}--{}); ".format(person.name,person.born,person.died)
+                    else:
+                        persons += "{} ({}--); ".format(person.name,person.born)
+                else:
+                    if person.died is not None:
+                        persons += "{} (--{}); ".format(person.name,person.died)
+                    else:
+                        persons += "{}; ".format(person.name)
+                
+            print("Composer: " + persons[:-2])
+        if self.edition.composition.name is not None:
+            print("Title: " + self.edition.composition.name)
+        if self.edition.composition.genre is not None:
+            print("Genre: " + self.edition.composition.genre)
+        if self.edition.composition.key is not None:
+            print("Key: " + self.edition.composition.key)
+        if self.edition.composition.year is not None:
+            print("Compostion Year: " + str(self.edition.composition.year))
+        if self.edition.name is not None:
+            print("Edition: " + self.edition.name)
+        if self.edition.authors:
+            authors = ""
+            for author in self.edition.authors:
+                authors += "{}, ".format(author.name)
+            print("Editor: " + authors[:-2])
+        if self.edition.composition.voices:
+            for i in range(len(self.edition.composition.voices)):
+                voice = self.edition.composition.voices[i]
+                if voice.range is not None:
+                    print("Voice {}: {}, {}".format(i+1,voice.range,voice.name))
+                else:
+                    print("Voice {}: {}".format(i+1,voice.name))
+        if self.partiture:
+            print("Partiture: yes")
+        else:
+            print("Partiture: no")
+        if self.edition.composition.incipit is not None:
+            print("Incipit: " + self.edition.composition.incipit)
 
     def composition(self):
-        pass
+        return self.edition.composition
 
 
 class Edition:
