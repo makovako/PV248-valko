048404c6877a24b2348000ced26f9a743330b386
Marek Valko <valko.marek@gmail.com>
2018-10-15 20:04:53 +0200

    tried using executescript

--- 03-database/pokus.py
+++ 03-database/pokus.py
@@ -4,6 +4,8 @@ import sqlite3
 
 conn = sqlite3.connect("scorelib.dat")
 cur = conn.cursor()
-# cur.execute("CREATE TABLE person ( id integer primary key not null, born integer, died integer, name varchar not null);")
-cur.execute("INSERT INTO person (id,born,died,name) VALUES (?,?,?,?)",(5,2000,2001,"Bach"))
+script = open("scorelib.sql","r")
+cur.executescript(script.read())
+script.close
 conn.commit()
+conn.close()
