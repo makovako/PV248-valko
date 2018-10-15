6e2af6445a47f9622cee22fd6de71531f1f7b1d2
Marek Valko <valko.marek@gmail.com>
2018-10-15 21:36:20 +0200

    experiments with select

--- 03-database/pokus.py
+++ 03-database/pokus.py
@@ -2,10 +2,22 @@
 
 import sqlite3
 
-conn = sqlite3.connect("scorelib.dat")
+conn = sqlite3.connect("test.dat")
 cur = conn.cursor()
-script = open("scorelib.sql","r")
-cur.executescript(script.read())
-script.close
+# script = open("scorelib.sql","r")
+# cur.executescript(script.read())
+# script.close
+cur.execute("drop table person;")
+cur.execute("create table person ( id integer primary key not null, name varchar not null );")
+cur.execute("insert into person (name) values (?)",("adam",))
+# print(cur.fetchall())
+cur.execute("insert into person (name) values (?)",("boris",))
+# print(cur.rowcount)
+cur.execute("insert into person (name) values (?)",("adam",))
+# print(cur.rowcount)
+cur.execute("select id from person where name = 'adam'")
+# print(cur.fetchall())
+for i in cur.fetchall():
+    print(i[0])
 conn.commit()
 conn.close()
