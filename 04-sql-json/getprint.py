82d4f68ac4b9b7d4c195f9ec491d9ac3ff7f76d2
Marek Valko <valko.marek@gmail.com>
2018-10-17 19:20:16 +0200

    part one done

--- 04-sql-json/getprint.py
+++ 04-sql-json/getprint.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+
+import sys, sqlite3
+import json
+
+conn = sqlite3.connect("scorelib.dat")
+cur = conn.cursor()
+cur.execute("select person.name,born,died from person join score_author on person.id = score_author.composer join score on score.id = score_author.score join edition on edition.score = score.id join print on print.edition=edition.id where print.id = ?",(int(sys.argv[1]),))
+persons = cur.fetchall()
+
+out = []
+for person in persons:
+    dic = {}
+    dic["name"] = person[0]
+    if person[1] is not None:
+        dic["born"] = person[1]
+    if person[2] is not None:
+        dic["died"] = person[2]
+    out.append(dic)
+print(json.dumps(out,indent=4,ensure_ascii=False))
+
+conn.commit()
+conn.close()
