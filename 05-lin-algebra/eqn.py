65fdbc1f6dff4c1c61bd267010ef82ce153d3693
Marek Valko <valko.marek@gmail.com>
2018-10-25 12:02:19 +0200

    Clean up, added comments

--- 05-lin-algebra/eqn.py
+++ 05-lin-algebra/eqn.py
@@ -10,10 +10,10 @@ lines = [] # list of dicts with variables and their coeficients e.g. [{"x":1,"y"
 variables = set() # set of all used variables
 
 for line in open(sys.argv[1],"r"):
-    left, right = line.strip().split("=")
+    left, right = line.strip().split("=") # left side coeficients and variables, right side constants
     constants.append(int(right.strip()))
     left.strip()    
-    positive = True
+    positive = True # if the number should be positive or negative, first number is always positive
     number = ""
     line_variables = {} # variables and their coeficients e. g. {"x":12,"y":3}
     for char in left:
@@ -23,20 +23,20 @@ for line in open(sys.argv[1],"r"):
             positive = False
         elif ord(char) >= 97 and ord(char) <= 122: #its variable
             variables.add(char)
-            if number == "":
+            if number == "": # coeficient is 1 if there is none near variable
                 number = "1"
-            if not positive:
+            if not positive: # create negative number
                 number = "-" + number
             
             line_variables[char] = int(number)
             number = ""
         elif ord(char) >= 48 and ord(char) <= 57:
-            number += char
-        else:
+            number += char # crate number by concatinating chars
+        else: # spaces, other garbage
             continue
     lines.append(line_variables)
 
-variables = list(variables)
+variables = list(variables) # convert to list so i can sort
 variables.sort()
 
 # create coeficients matrix
@@ -49,32 +49,30 @@ for line in lines:
             coeficient_line.append(0)
     coeficients.append(coeficient_line)
 
+# math stuff
 augmented_matrix = np.column_stack((coeficients,constants))
 coeficients_rank = np.linalg.matrix_rank(coeficients)
 augmented_rank = np.linalg.matrix_rank(augmented_matrix)
 dimension = len(variables) - coeficients_rank
 
+# helper prints for debuging
 debug = False
 if debug:
     print("Variables set sorted: {}".format(variables))
     print("Parsed equations: {}".format(lines))
     print("Coef_rank: {}, Aug_rank: {}".format(coeficients_rank,augmented_rank))
-
     print("Coeficients: {}".format(coeficients))
     print("Augmented matrix: {}".format(augmented_matrix))
 
 if coeficients_rank != augmented_rank:
     print("no solution")
-else:
+else: 
     if coeficients_rank == len(variables):
-        # print solution TODO
         answer = ""
-        solution = np.linalg.solve(coeficients,constants)
+        solution = np.linalg.solve(coeficients,constants) # returns list of floats
         for i in range(len(variables)):
+            # if solution is integer, convert ot integer, otherwise stays float
             answer += " {} = {},".format(variables[i],int(solution[i]) if solution[i].is_integer() else solution[i])
-        print("solution:{}".format(answer[:-1]))
+        print("solution:{}".format(answer[:-1])) # [:-1], there is "," at the end
     else:
         print("solution space dimension: {}".format(dimension))
-
-
-
