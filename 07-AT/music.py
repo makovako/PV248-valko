2573bf34802a7ffaef8c726e013f02e2431baef4
Marek Valko <valko.marek@gmail.com>
2018-11-06 17:47:21 +0100

    task 7 base

---
+++ 07-AT/music.py
@@ -0,0 +1,11 @@
+#!/usl/bin/python3
+
+#3 najvacsie amplitudy tych peakov
+# vyber peaky viac ako priemer20
+# zoradit podla ampltudy
+#vybrat prvu
+# zahodit co je kolem - clustrovani
+# a vyberam dalsie
+#vyberam amplitudu a z nej potrebujem vypisujem frekvence
+# ak sa opakuju po oknach tak vznika segment, musi sediet pocet aj ton
+
