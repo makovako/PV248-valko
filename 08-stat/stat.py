91fdfbe72d727eae8db479806028e07ce6e17f4e
Marek Valko <valko.marek@gmail.com>
2018-11-20 12:27:28 +0100

    stat py

--- 08-stat/stat.py
+++ 08-stat/stat.py
@@ -1,41 +1,73 @@
 #!/usr/bin/python3
-import csv, sys
+import csv, sys, json
 import pandas as pd
 
-df = pd.read_csv("points.csv")
-
-input_file = sys.argv[1]
+df = pd.read_csv(sys.argv[1])
 mode = sys.argv[2]
-
-# print(df.columns)
-
 output = {}
 if mode == "dates":
-    dates = []
-    for line in open(input_file,"r"):
-        dates.append(line)
+    dates = set()
+    headers = df.columns.values[1:]
+    dateheader = {}
+    for header in headers:         
+        dates.add(header.split("/")[0].strip())
+    dates = list(dates)
+    dates.sort()
     for date in dates:
-        headers = []
-        for header in df.columns:         
+        dateheader[date] = []
+    for date in dates:
+        for header in headers:
             if date in header:
-                headers.append(header)
-        mean = df[headers].mean()
-        print(mean)
-        median = df[headers].median().median()
-        last = df[headers]
-        first = df[headers].quantile(0.75)
-        print(median)
-        print(last)
-        print(first)
-        
-            
-        
-    
-    pass    
+                dateheader[date].append(header)
+    for k,v in dateheader.items():
+        x = []
+        for col in v:
+            x.append(df[col])
+        data = pd.concat(x)
+        out = {}
+        out["mean"] = data.mean()
+        out["median"] = data.median()
+        out["passed"] = len(data[data > 0])
+        out["first"] = data.quantile(0.25)
+        out["last"] = data.quantile(0.75)
+        output[k.strip()] = out
 if mode == "deadlines":
-    pass
-if mode == "exercises":
-    pass
-
-
+    headers = df.columns.values[1:]
+    for header in headers:
+        out = {}
+        out["mean"] = df[header].mean()
+        out["median"] = df[header].median()
+        out["passed"] = len(df[df[header]>0])
+        out["first"] = df[header].quantile(0.25)
+        out["last"] = df[header].quantile(0.75)
+        output[header.strip()] = out
     
+if mode == "exercises":
+    exercises = set()
+    headers = df.columns.values[1:]
+    exheader = {}
+    for header in headers:
+        exercises.add(header.split("/")[1].strip())
+    exercises = list(exercises)
+    exercises.sort()
+    for ex in exercises:
+        exheader[ex] = []
+    for ex in exercises:
+        for header in headers:
+            if "/"+ex in header:
+                exheader[ex].append(header)
+    for k,v in exheader.items():
+        x = []
+        for col in v:
+            x.append(df[col])
+        data = pd.concat(x)
+        out = {}
+        out["mean"] = data.mean()
+        out["median"] = data.median()
+        out["passed"] = len(data[data > 0])
+        out["first"] = data.quantile(0.25)
+        out["last"] = data.quantile(0.75)
+        output[k.strip()] = out
+        
+
+print(json.dumps(output,indent=4,ensure_ascii=False))
