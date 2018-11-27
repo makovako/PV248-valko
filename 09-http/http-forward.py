a3a26500a03dd66f809da6f1b16b81600013be5e
Marek Valko <valko.marek@gmail.com>
2018-11-27 21:05:24 +0100

    week 9 get method

--- 09-http/http-forward.py
+++ 09-http/http-forward.py
@@ -2,39 +2,48 @@
 
 import sys
 import http
+import json
+from socket import timeout
 
 port = int(sys.argv[1])
 upstream = sys.argv[2]
 from http.server import BaseHTTPRequestHandler,HTTPServer
-from http.client import HTTPConnection
+import urllib
 
-#This class will handles any incoming request from
-#the browser 
 class myHandler(BaseHTTPRequestHandler):
 	
-	#Handler for the GET requests
 	def do_GET(self):
-		httpconnection = HTTPConnection(upstream,timeout=1)
-		httpconnection.request("GET",upstream)
-		r1 = httpconnection.getresponse()
-		print(r1.status,r1.reason)
-		# self.request()
+		headers = dict(self.headers)
+		request = urllib.request.Request(upstream)
+		request.headers = headers
+		request.method = 'GET'
+		out = {}
+		try:
+			with urllib.request.urlopen(request,timeout=1) as response:
+				out["code"] = response.status
+				out["headers"]	= dict(response.getheaders())
+				data = response.read()
+				try:
+					out["json"] = json.loads(data)
+				except ValueError:
+					out["content"] = data
+		except timeout:
+			out["code"] = "timeout"
 		self.send_response(200)
-		self.send_header('Content-type','text/html')
+		self.send_header("Content-Type",'application/json')
 		self.end_headers()
-		# Send the html message
-		self.wfile.write(bytes("Hello World !\n","utf8"))
+		self.wfile.write(bytes(json.dumps(out, indent=4, ensure_ascii = False), 'UTF-8'))
 		return
 
 try:
 	#Create a web server and define the handler to manage the
 	#incoming request
 	server = HTTPServer(('', port), myHandler)
-	print('Started httpserver on port ' , port)
+	# print('Started httpserver on port ' , port)
 	
 	#Wait forever for incoming htto requests
 	server.serve_forever()
 
 except KeyboardInterrupt:
-	print ('^C received, shutting down the web server')
+	# print ('^C received, shutting down the web server')
 	server.socket.close()
